40e60a87a7bb1a0623dc0cce69957b76130c178c
adelina2222 <adelinachiriac@gmail.com>
2021-08-17 19:59:40 +0100

    Install & require the Caporal module

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const debounce = require("lodash.debounce");
 // Require the Chokidar module
 const chokidar = require("chokidar");
 
+// Require the Caporal module
+const program = require("caporal");
+
 // Add a debounce function to the add event, in order to counteract the chokidar initial file observation, which triggered the add event for each existing file
 const startProgram = debounce(() => {
     console.log("A file has been added!");
