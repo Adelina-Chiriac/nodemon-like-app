8fff217334ddb64916be3f59d3c3ebca8dbd657d
adelina2222 <adelinachiriac@gmail.com>
2021-08-17 17:57:05 +0100

    Add a debounce function to the add event

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const debounce = require("lodash-debounce");
 // Require the Chokidar module
 const chokidar = require("chokidar");
 
+// Add a debounce function to the add event, in order to counteract the chokidar initial file observation, which triggered the add event for each existing file
+const startProgram = debounce(() => {
+    console.log("A file has been added!");
+}, 100);
+
 chokidar.watch(".")
-    .on("add", () => {
-        console.log("A file has been added!");
-    })
+    .on("add", startProgram)
     .on("change", () => {
         console.log("The file has been changed!");
     })
