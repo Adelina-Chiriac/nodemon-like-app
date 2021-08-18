9e78385a2361a49114c2969252188ffa873f2e07
adelina2222 <adelinachiriac@gmail.com>
2021-08-18 12:47:09 +0100

    Require the fs module, set the value for the name of the file variable & check to see if it exists in the directory

--- index.js
+++ index.js
@@ -9,10 +9,22 @@ const chokidar = require("chokidar");
 // Require the Caporal module
 const program = require("caporal");
 
+// Require the File System module
+const fs = require("fs");
+
 program
     .version("0.0.1")
     .argument("[filename]", "Name of a file to execute")
-    .action((args) => {
+    .action(async (args) => {
+        // Destructure the args object to extract the filename
+        const { filename } = args;
+
+        // Check to see if the user provided a filename in the arguments, and if not, default to index.js
+        const name = filename || "index.js";
+
+        // Check to see if this filename actually exists
+        await fs.promises.access(name);
+
         // Add a debounce function to the add event, in order to counteract the chokidar initial file observation, which triggered the add event for each existing file
         const startProgram = debounce(() => {
             console.log("A file has been added!");
