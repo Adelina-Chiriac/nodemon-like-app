f8147063cb5a2bd5a1b25eae0b9d31737e609df6
adelina2222 <adelinachiriac@gmail.com>
2021-08-18 12:27:07 +0100

    Start implementing the caporal module

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const chokidar = require("chokidar");
 // Require the Caporal module
 const program = require("caporal");
 
+program
+    .version("0.0.1")
+    .argument("[filename]", "Name of a file to execute")
+    .action((args) => {
+        console.log(args);
+    });
+
+program.parse(process.argv);
+
 // Add a debounce function to the add event, in order to counteract the chokidar initial file observation, which triggered the add event for each existing file
 const startProgram = debounce(() => {
     console.log("A file has been added!");
